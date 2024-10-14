90c92ba60a9666f3ef6fc780949b98a6a07979a4
thotvte <thotvpd07294@fpt.edu.vn>
2024-10-14 17:14:02 +0700

    modules schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { AppController } from "@/app.controller";
+import { AppService } from "@/app.service";
+import { UsersModule } from "@/modules/users/users.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ReviewsModule } from "@/modules/reviews/reviews.module";
+import { RestaurantsModule } from "@/modules/restaurants/restaurants.module";
+import { OrdersModule } from "@/modules/orders/orders.module";
+import { OrderDetailModule } from "@/modules/order.detail/order.detail.module";
+import { MenusModule } from "@/modules/menus/menus.module";
+import { MenuItemsModule } from "@/modules/menu.items/menu.items.module";
+import { MenuItemOptionsModule } from "@/modules/menu.item.options/menu.item.options.module";
+import { LikesModule } from "@/modules/likes/likes.module";
 
 @Module({
   imports: [
@@ -12,10 +20,18 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
+        uri: configService.get<string>("MONGODB_URI"),
       }),
       inject: [ConfigService],
     }),
+    ReviewsModule,
+    RestaurantsModule,
+    OrdersModule,
+    OrderDetailModule,
+    MenusModule,
+    MenuItemsModule,
+    MenuItemOptionsModule,
+    LikesModule,
   ],
 
   controllers: [AppController],
