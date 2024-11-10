b874576b1b8a404be451fe6f73a93302e3cb5a56
thotvte <thotvpd07294@fpt.edu.vn>
2024-11-10 19:17:22 +0700

    send gmail with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,18 +15,12 @@ import { LikesModule } from "@/modules/likes/likes.module";
 import { AuthModule } from "@/auth/auth.module";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./auth/passport/jwt-auth.guard";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { HandlebarsAdapter } from "@nestjs-modules/mailer/dist/adapters/handlebars.adapter";
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>("MONGODB_URI"),
-      }),
-      inject: [ConfigService],
-    }),
     ReviewsModule,
     RestaurantsModule,
     OrdersModule,
@@ -36,6 +30,42 @@ import { JwtAuthGuard } from "./auth/passport/jwt-auth.guard";
     MenuItemOptionsModule,
     LikesModule,
     AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("MONGODB_URI"),
+      }),
+      inject: [ConfigService],
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          // ignoreTLS: true,
+          // secure: false,
+          auth: {
+            user: configService.get<string>("MAIL_USER"),
+            pass: configService.get<string>("MAIL_PASSWORD"),
+          },
+        },
+        defaults: {
+          from: '"No Reply" <no-reply@localhost>',
+        },
+        // preview: true,
+        // template: {
+        //   dir: process.cwd() + "/template/",
+        //   adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+        //   options: {
+        //     strict: true,
+        //   },
+        // },
+      }),
+      inject: [ConfigService],
+    }),
   ],
 
   controllers: [AppController],
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,10 +15,14 @@ import { LocalAuthGuard } from "./passport/local-auth.guard";
 import { JwtAuthGuard } from "./passport/jwt-auth.guard";
 import { Public } from "@/decorator/customize";
 import { CreateAuthDto } from "./dto/create-auth.dto";
+import { MailerService } from "@nestjs-modules/mailer";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Public()
@@ -32,5 +36,18 @@ export class AuthController {
   @Public()
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get("mail")
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: "gtet721@gmail.com", // list of receivers
+      // from: "noreply@nestjs.com", // sender address
+      subject: "Testing Nest MailerModule ✔", // Subject line
+      text: "welcome", // plaintext body
+      html: "<b>Hello word Tho dep trai</b>", // HTML body content
+    });
+    return "oke";
   }
 }
